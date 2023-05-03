9760ffea1ec571e41c4ebba5434f9c09ce96dab7
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-05-03 10:38:40 +0200

    Finished login system. Improved comment container.

---
+++ pages/account.js
@@ -0,0 +1,15 @@
+// Libraries
+import Head from 'next/head'
+
+const Account = () => {
+  return (
+    <>
+      <Head>
+        <title>Bruger</title>
+      </Head>
+      Hej!
+    </>
+  )
+}
+
+export default Account;
